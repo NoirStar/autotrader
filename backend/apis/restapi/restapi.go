5e2148f870747db45a830d39c4317a2f6f5ba18c
namwoo.kim <sky_9233@naver.com>
2021-05-27 09:24:59 +0900

    modeling methods

--- backend/apis/restapi/restapi.go
+++ backend/apis/restapi/restapi.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"net/url"
 
+	"github.com/noirstar/autotrading/backend/models"
 	"github.com/noirstar/autotrading/backend/utils/env"
 	"github.com/noirstar/autotrading/backend/utils/jwt"
 	"github.com/noirstar/autotrading/backend/utils/myerr"
@@ -24,88 +25,93 @@ func GetAccount(accessKey string, secretKey string) []byte {
 }
 
 // GetOrderChance 주문 가능 정보 - 마켓별 주문 가능 정보 확인
-func GetOrderChance(accessKey string, secretKey string, query map[string]interface{}) []byte {
+func GetOrderChance(accessKey string, secretKey string, query *models.ReqChance) []byte {
 
 	reqURL := baseURL + "/v1/orders/chance"
-	tokenString := jwt.GetJwtTokenWithQuery(accessKey, secretKey, query)
+	queryMap := ConvertStructToMap(query)
+	tokenString := jwt.GetJwtTokenWithQuery(accessKey, secretKey, queryMap)
 
-	return RequestToServer(reqURL, "GET", tokenString, query)
+	return RequestToServer(reqURL, "GET", tokenString, queryMap)
 }
 
 // GetOrderSearch 개별 주문 조회 - 주문 UUID 를 통해 개별 주문건을 조회
-func GetOrderSearch(accessKey string, secretKey string, query map[string]interface{}) []byte {
+func GetOrderSearch(accessKey string, secretKey string, query *models.ReqOrderSearch) []byte {
 
 	reqURL := baseURL + "/v1/order"
-	tokenString := jwt.GetJwtTokenWithQuery(accessKey, secretKey, query)
+	queryMap := ConvertStructToMap(query)
+	tokenString := jwt.GetJwtTokenWithQuery(accessKey, secretKey, queryMap)
 
-	return RequestToServer(reqURL, "GET", tokenString, query)
+	return RequestToServer(reqURL, "GET", tokenString, queryMap)
 }
 
 // GetOrdersSearch 주문 리스트 조회 - 주문 리스트를 조회
-func GetOrdersSearch(accessKey string, secretKey string, query map[string]interface{}) []byte {
+func GetOrdersSearch(accessKey string, secretKey string, query *models.ReqOrdersSearch) []byte {
 
 	reqURL := baseURL + "/v1/orders"
-	tokenString := jwt.GetJwtTokenWithQuery(accessKey, secretKey, query)
+	queryMap := ConvertStructToMap(query)
+	tokenString := jwt.GetJwtTokenWithQuery(accessKey, secretKey, queryMap)
 
-	return RequestToServer(reqURL, "GET", tokenString, query)
+	return RequestToServer(reqURL, "GET", tokenString, queryMap)
 }
 
 // DeleteOrder 주문 취소 접수 - 주문 UUID를 통해 해당 주문에 대한 취소 접수
-func DeleteOrder(accessKey string, secretKey string, query map[string]interface{}) []byte {
+func DeleteOrder(accessKey string, secretKey string, query *models.ReqDeleteOrder) []byte {
 
 	reqURL := baseURL + "/v1/order"
-	tokenString := jwt.GetJwtTokenWithQuery(accessKey, secretKey, query)
+	queryMap := ConvertStructToMap(query)
+	tokenString := jwt.GetJwtTokenWithQuery(accessKey, secretKey, queryMap)
 
-	return RequestToServer(reqURL, "DELETE", tokenString, query)
+	return RequestToServer(reqURL, "DELETE", tokenString, queryMap)
 }
 
 // PostOrder 주문하기
-func PostOrder(accessKey string, secretKey string, query map[string]interface{}) []byte {
+func PostOrder(accessKey string, secretKey string, query *models.ReqOrders) []byte {
 
 	reqURL := baseURL + "/v1/orders"
-	tokenString := jwt.GetJwtTokenWithQuery(accessKey, secretKey, query)
+	queryMap := ConvertStructToMap(query)
+	tokenString := jwt.GetJwtTokenWithQuery(accessKey, secretKey, queryMap)
 
-	return RequestToServer(reqURL, "POST", tokenString, query)
+	return RequestToServer(reqURL, "POST", tokenString, queryMap)
 }
 
 // GetMarketCode 마켓 코드 조회 - 업비트에서 거래 가능한 마켓 목록
-func GetMarketCode(query map[string]interface{}) []byte {
+func GetMarketCode() []byte {
 
-	reqURL := baseURL + "/v1/market/all"
+	reqURL := baseURL + "/v1/market/all?isDetails=true"
 
-	return RequestToServerSimple(reqURL, "GET", query)
+	return RequestToServerSimple(reqURL, "GET", nil)
 }
 
 // GetMinuteCandles 분 캔들 조회
-func GetMinuteCandles(query map[string]interface{}, unit string) []byte {
+func GetMinuteCandles(query *models.ReqMinuteCandles, unit string) []byte {
 
 	reqURL := baseURL + "/v1/candles/minutes/" + unit
 
-	return RequestToServerSimple(reqURL, "GET", query)
+	return RequestToServerSimple(reqURL, "GET", ConvertStructToMap(query))
 }
 
 // GetDayCandles 일 캔들 조회
-func GetDayCandles(query map[string]interface{}) []byte {
+func GetDayCandles(query *models.ReqDayCandles) []byte {
 
 	reqURL := baseURL + "/v1/candles/days"
 
-	return RequestToServerSimple(reqURL, "GET", query)
+	return RequestToServerSimple(reqURL, "GET", ConvertStructToMap(query))
 }
 
 // GetWeekCandles 주 캔들 조회
-func GetWeekCandles(query map[string]interface{}) []byte {
+func GetWeekCandles(query *models.ReqWeekCandles) []byte {
 
 	reqURL := baseURL + "/v1/candles/weeks"
 
-	return RequestToServerSimple(reqURL, "GET", query)
+	return RequestToServerSimple(reqURL, "GET", ConvertStructToMap(query))
 }
 
 // GetMonthsCandles 달 캔들 조회
-func GetMonthsCandles(query map[string]interface{}) []byte {
+func GetMonthsCandles(query *models.ReqMonthCandles) []byte {
 
 	reqURL := baseURL + "/v1/candles/months"
 
-	return RequestToServerSimple(reqURL, "GET", query)
+	return RequestToServerSimple(reqURL, "GET", ConvertStructToMap(query))
 }
 
 // RequestToServer 업비트 서버로 요청
