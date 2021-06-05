023f75abab3f8636befabdc4ad67f4ac696d109a
namwoo.kim <sky_9233@naver.com>
2021-06-05 23:02:15 +0900

    test case

--- backend/main.go
+++ backend/main.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/noirstar/autotrading/backend/apis/restapi"
 	"github.com/noirstar/autotrading/backend/models"
 	"github.com/noirstar/autotrading/backend/utils/myerr"
@@ -15,9 +16,6 @@ import (
 
 func main() {
 
-	//accessKey := env.GetEnv("UPBIT_ACCESS_KEY")
-	//secretKey := env.GetEnv("UPBIT_SECRET_KEY")
-
 	req := models.ReqMinuteCandles{
 		Market: "KRW-BTC",
 		Count:  200,
@@ -26,60 +24,80 @@ func main() {
 
 	reqText := restapi.GetMinuteCandles(&req, 1)
 	json.Unmarshal(reqText, &res)
-	fmt.Println(res[0].TradePrice)
-	series := techan.NewTimeSeries()
-	for i := len(res) - 1; i >= 0; i-- {
-		layout := strings.Split(time.RFC3339, "Z")[0]
-		start, err := time.Parse(layout, res[i].CandleDateTimeKST)
-		myerr.CheckErr(err)
-		period := techan.NewTimePeriod(start, time.Minute)
 
-		candle := techan.NewCandle(period)
-		candle.OpenPrice = big.NewFromString(fmt.Sprintf("%v", res[i].OpeningPrice))
-		candle.ClosePrice = big.NewFromString(fmt.Sprintf("%v", res[i].TradePrice))
-		candle.MaxPrice = big.NewFromString(fmt.Sprintf("%v", res[i].HighPrice))
-		candle.MinPrice = big.NewFromString(fmt.Sprintf("%v", res[i].LowPrice))
+	series := MakeSeries(res)
 
-		series.AddCandle(candle)
-	}
-
-	//closePrices := techan.NewClosePriceIndicator(series)
-	//movingAverage := techan.NewSimpleMovingAverage(closePrices, 10)
-
-	indicator := techan.NewClosePriceIndicator(series)
-
-	order := techan.Order{
-		Side:          techan.BUY,
-		Security:      "what",
-		Price:         big.NewDecimal(res[0].TradePrice),
-		Amount:        big.NewDecimal(1),
-		ExecutionTime: time.Now(),
-	}
+	closePrices := techan.NewClosePriceIndicator(series)
+	movingAverage := techan.NewSimpleMovingAverage(closePrices, 10)
 
 	record := techan.NewTradingRecord()
-	record.Operate(order)
-
-	entryConstant := techan.NewConstantIndicator(30)
-	exitConstant := techan.NewConstantIndicator(10)
+	// record.Operate(order)
 
 	entryRule := techan.And(
-		techan.NewCrossUpIndicatorRule(entryConstant, indicator),
+		// 종가가 ma를 넘어서거나, 새 포지션일때
+		techan.NewCrossUpIndicatorRule(movingAverage, closePrices),
 		techan.PositionNewRule{})
 
 	exitRule := techan.And(
-		techan.NewCrossDownIndicatorRule(indicator, exitConstant),
+		// 종가가 ma 밑에 오게될때,
+		techan.NewCrossDownIndicatorRule(closePrices, movingAverage),
 		techan.PositionOpenRule{})
 
 	strategy := techan.RuleStrategy{
-		UnstablePeriod: 10,
+		UnstablePeriod: 5,
 		EntryRule:      entryRule,
 		ExitRule:       exitRule,
+	}
+	account := techan.BuyAndHoldAnalysis{
+		TimeSeries:    series,
+		StartingMoney: 1000000,
 	}
 
 	for i := 0; i < len(res); i++ {
 		if strategy.ShouldEnter(i, record) {
-			fmt.Println(i, strategy.ShouldEnter(i, record))
+			fmt.Println("Enter : ", i, "MA : ", movingAverage.Calculate(i).FormattedString(0), "Price : ", closePrices.Calculate(i).FormattedString(0))
+			order := techan.Order{
+				Side:          techan.BUY,
+				Security:      uuid.NewString(),
+				Price:         closePrices.Calculate(i),
+				Amount:        big.NewDecimal(0.1),
+				ExecutionTime: time.Now(),
+			}
+			record.Operate(order)
+		} else if strategy.ShouldExit(i, record) {
+			fmt.Println("Exit : ", i, "MA : ", movingAverage.Calculate(i).FormattedString(0), "Price : ", closePrices.Calculate(i).FormattedString(0))
+			order := techan.Order{
+				Side:          techan.SELL,
+				Security:      uuid.NewString(),
+				Price:         closePrices.Calculate(i),
+				Amount:        big.NewDecimal(0.1),
+				ExecutionTime: time.Now(),
+			}
+			record.Operate(order)
 		}
-	}
 
+	}
+	fmt.Println(account.Analyze(record))
+
+}
+
+// MakeSeries makes new series (list of candles)
+func MakeSeries(candles []models.ResMinuteCandles) *techan.TimeSeries {
+
+	series := techan.NewTimeSeries()
+	for i := len(candles) - 1; i >= 0; i-- {
+		layout := strings.Split(time.RFC3339, "Z")[0]
+		start, err := time.Parse(layout, candles[i].CandleDateTimeKST)
+		myerr.CheckErr(err)
+		period := techan.NewTimePeriod(start, time.Minute)
+
+		candle := techan.NewCandle(period)
+		candle.OpenPrice = big.NewFromString(fmt.Sprintf("%v", candles[i].OpeningPrice))
+		candle.ClosePrice = big.NewFromString(fmt.Sprintf("%v", candles[i].TradePrice))
+		candle.MaxPrice = big.NewFromString(fmt.Sprintf("%v", candles[i].HighPrice))
+		candle.MinPrice = big.NewFromString(fmt.Sprintf("%v", candles[i].LowPrice))
+
+		series.AddCandle(candle)
+	}
+	return series
 }
