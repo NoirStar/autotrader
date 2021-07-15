e1cd282c4e6bdffc2370209693fdfdde3135c49f
namwoo.kim <sky_9233@naver.com>
2021-07-16 00:20:55 +0900

    test

--- frontend/src/components/LoginForm.spec.js
+++ frontend/src/components/LoginForm.spec.js
@@ -5,5 +5,6 @@ describe('LoginForm.vue', () => {
   test('컴포넌트가 마운팅되면 화면에 그려져야 한다', () => {
     const wrapper = shallowMount(LoginForm);
     expect(wrapper.vm.id).toBe('');
+    wrapper.find('');
   });
 });
